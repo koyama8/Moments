89d6fb91896489d4812b8bd96d2d254d167a12ca
Matheus Koyama <koyamaCRS@gmail.com>
2024-08-02 09:10:11 -0300

    Finalizando o sistema de mensagem

--- src/app/components/pages/new-moment/new-moment.component.ts
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Moment } from 'src/app/Moment';
 import { MomentService } from 'src/app/services/moment.service';
+import { MessagesService } from 'src/app/services/messages.service';
 
 @Component({
   selector: 'app-new-moment',
@@ -11,7 +13,10 @@ export class NewMomentComponent implements OnInit {
 
   btnText = 'Compartilhar!';
 
-  constructor(private momentService: MomentService) { }
+  constructor(
+    private momentService: MomentService,
+    private messagesService:MessagesService,
+    private router: Router) { }
 
   ngOnInit(): void {
   }
@@ -26,8 +31,12 @@ export class NewMomentComponent implements OnInit {
       formData.append('image', moment.image);
     }
 
+
     await this.momentService.createMoment(formData).subscribe();
 
-  }
+    this.messagesService.add('Momento adicionado com sucesso!')
 
+    this.router.navigate(['']);
+
+  }
 }
