3facdde4082b9212659834a9decab377dff118ae
Matheus Koyama <koyamaCRS@gmail.com>
2024-08-02 12:04:40 -0300

    CSS na nágina de Momentos

--- src/app/components/pages/moment/moment.component.ts
+++ src/app/components/pages/moment/moment.component.ts
@@ -6,6 +6,10 @@ import { Moment } from 'src/app/Moment';
 
 import { Router, ActivatedRoute } from '@angular/router';
 
+import { environment } from 'src/environments/environment';
+
+import { faTimes, faEdit } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-moment',
   templateUrl: './moment.component.html',
@@ -14,6 +18,11 @@ import { Router, ActivatedRoute } from '@angular/router';
 export class MomentComponent implements OnInit {
 
   moment?: Moment;
+  baseApiUrl = environment.baseApiUrl;
+
+  faTimes = faTimes;
+  faEdit = faEdit;
+
 
   constructor(private momentService: MomentService, private route: ActivatedRoute) { }
 
