d72a6a16e57e75b768ae3a2d862d0ef8b4c406ce
Matheus Koyama <koyamaCRS@gmail.com>
2024-08-02 10:08:52 -0300

    Resgatando dados no banco e ajustes gerais no código

--- src/app/services/moment.service.ts
+++ src/app/services/moment.service.ts
@@ -4,6 +4,8 @@ import { Observable } from 'rxjs';
 
 import { Moment } from '../Moment';
 
+import { Response } from '../Response';
+
 import { environment } from 'src/environments/environment';
 
 
@@ -17,6 +19,11 @@ export class MomentService {
 
   constructor(private http: HttpClient) { }
 
+  getMoments(): Observable<Response<Moment[]>> {
+    return this.http.get<Response<Moment[]>>(this.apiUrl);
+  }
+
+  
   createMoment(formData: FormData): Observable<FormData> {
     return this.http.post<FormData>(this.apiUrl, formData);
   }
