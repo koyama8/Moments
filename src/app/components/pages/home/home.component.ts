cd60a3d82905fcb70dc345f5ed3ea1db09798538
Matheus Koyama <koyamaCRS@gmail.com>
2024-09-14 11:03:12 -0300

    Resolvido conflito após pull

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,10 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MomentService } from 'src/app/services/moment.service';
-
 import { Moment } from 'src/app/Moment';
-
 import { environment } from 'src/environments/environment';
-
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -14,15 +11,13 @@ import { faSearch } from '@fortawesome/free-solid-svg-icons';
 })
 export class HomeComponent implements OnInit {
 
-  allMoments: Moment[] = []
-  moments: Moment[] = []
-  baseApiUrl = environment.baseApiUrl
+  allMoments: Moment[] = [];
+  moments: Moment[] = [];
+  baseApiUrl = environment.baseApiUrl;
 
   //todo search
-  faSearch = faSearch
+  faSearch = faSearch;
   searchTerm: string = '';
-
-
 
   constructor(private momentService: MomentService) { }
 
@@ -39,6 +34,32 @@ export class HomeComponent implements OnInit {
       this.allMoments = items.data;
       this.moments = items.data;
     });
+
+    // Chama a função para digitar automaticamente no input
+    this.autoType();
+  }
+
+  // Função para digitar automaticamente no placeholder do campo de busca
+  autoType() {
+    const typingText = 'Ou busque por um momento...'; // Texto que será digitado
+    const typingSpeed = 400; // Velocidade da "digitação" (em milissegundos)
+    let currentIndex = 0;
+
+    const inputElement: HTMLInputElement | null = document.querySelector('input[type="text"]');
+
+    if (inputElement) {
+      inputElement.placeholder = ''; // Inicia o placeholder vazio
+
+      const intervalId = setInterval(() => {
+        if (currentIndex < typingText.length) {
+          inputElement.placeholder += typingText.charAt(currentIndex);
+          currentIndex++;
+        } else {
+          currentIndex = 0;
+          inputElement.placeholder = ''; // Reinicia a digitação
+        }
+      }, typingSpeed);
+    }
   }
 
   search(e: Event): void {
@@ -49,5 +70,4 @@ export class HomeComponent implements OnInit {
       moment.title.toLowerCase().includes(value)
     );
   }
-
 }
